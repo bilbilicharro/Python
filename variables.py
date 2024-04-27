8394e8a5bc6c7629894a8c10f8f9987638c618e1
bilbilicharro <bilbilicharro@gmail.com>
2024-04-27 11:15:08 +0200

    Comienzo del proyecto

---
+++ variables.py
@@ -0,0 +1,15 @@
+# Comentario de una sola línea
+"""
+Variables
+
+En Python, las variables se utilizan para almacenar información. 
+Las variables pueden ser consideradas como contenedores que contienen datos que pueden ser cambiados 
+posteriormente a lo largo de la programación. No necesitas declarar el tipo de variable, se infiere 
+dinámicamente del valor que se le asigna.
+
+"""
+x = 5
+name = "John"
+
+print(f"Variable: {x}, Tipo de Variable {type(x)}")
+print(f"Variable: {name}, Tipo de Variable {type(name)}")
